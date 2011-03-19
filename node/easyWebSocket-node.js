5e512d2da43023be6d8cf2271c10418b6e382ea1
Jerome Etienne <jerome.etienne@gmail.com>
2011-03-19 17:46:02 +0100

    change nodester port to 8689 (trying to get it on nodester) 	modified:   node/easyWebSocket-node.js 	modified:   node/server/Makefile 	modified:   node/server/server.js

--- node/easyWebSocket-node.js
+++ node/easyWebSocket-node.js
@@ -134,7 +134,7 @@ EasyWebSocket.CLOSED		= 3;
  *   * EasyWebSocket.iframeOrigin	= "http://localhost:8080";
  *   * EasyWebSocket.logFunction	= console.log.bind(console);
 */
-EasyWebSocket.serverUrl		= "http://88.191.76.230:8667"; 	// TODO change this to be tunable and work on nodester
+EasyWebSocket.serverUrl		= "http://88.191.76.230:8689"; 	// TODO change this to be tunable and work on nodester
 EasyWebSocket.logFunction	= function(){}
 
 //////////////////////////////////////////////////////////////////////////////////
